90c7bff87349701934c30136cd418ec7ebb8f886
qsm <348867341@qq.com>
2016-07-30 21:04:34 +0800

    http

---
+++ 1.http/1.server.js
@@ -0,0 +1,26 @@
+var http = require('http')
+
+http.createServer((request, response) => {
+    console.log(request.method)    //请求的方法 (get/post)）
+    console.log(request.url)        //请求的url (/)
+    console.log(request.httpVersion)    //http版本号 (1.1)
+    console.log(request.headers)        //请求头的  请求头 
+
+//  { 
+//   host: 'localhost:9090',
+//   connection: 'keep-alive',
+//   'cache-control': 'max-age=0',
+//   'upgrade-insecure-requests': '1',
+//   'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36',
+//   accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+//   'accept-encoding': 'gzip, deflate, sdch',
+//   'accept-language': 'zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4' 
+// }
+
+    response.statusCode = 200           //设置状态吗
+    response.setHeader('name', 'qsm')   //设置响应头   key value形式
+    response.write('hellow')        //写入响应体    （可以多次写入）
+    response.write('world')
+    response.end()                  //结束本次响应
+})
+.listen(9090)
