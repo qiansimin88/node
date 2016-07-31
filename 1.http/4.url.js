e7a32030a615ba4e97531babe79ffb891b981094
qsm <348867341@qq.com>
2016-07-31 11:20:23 +0800

    url模块的作用

---
+++ 1.http/4.url.js
@@ -0,0 +1,27 @@
+var url = require('url'),
+    str = 'https://github.com:8082/qiansimin88/node?name=qsm&age=18#hehe'
+//url转成对象
+console.log(url.parse(str, true))
+
+// {
+//   protocol: 'https:',   协议
+//   slashes: true,    是否有//
+//   auth: null,    用户名和密码
+//   host: 'github.com:8082', 主机
+//   port: '8082',     端口
+//   hostname: 'github.com',   域名
+//   hash: '#hehe',            
+//   search: '?name=qsm&age=18',   
+//   query: 'name=qsm&age=18',     查询字符串
+//   pathname: '/qiansimin88/node',    路径
+//   path: '/qiansimin88/node?name=qsm&age=18', Pathname+search
+//   href: 'https://github.com:8082/qiansimin88/node?name=qsm&age=18#hehe' 原始url
+// }
+
+
+
+//对象转成url
+var urlobj = url.parse(str)
+console.log(url.format(urlobj))
+
+// https://github.com:8082/qiansimin88/node?name=qsm&age=18#hehe
