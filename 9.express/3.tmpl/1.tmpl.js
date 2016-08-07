09b9b4b994be2a8999a5c163b68fd6b865c1b032
qsm <348867341@qq.com>
2016-08-07 14:30:03 +0800

    模板引擎

---
+++ 9.express/3.tmpl/1.tmpl.js
@@ -0,0 +1,21 @@
+
+var express = require('express')
+var path = require('path')
+
+var app = express()
+
+// 指定模板引擎
+app.set('view engine', 'ejs')
+
+//设置视图的位置
+app.set('views', path.join(__dirname, '../4.views'))
+// console.log(path.join(__dirname, '../4.views'))
+
+app.get('/', (req, res) => {
+    res.render('1.index.ejs', {
+        title: 'qiasimin',
+        count: 3
+    })
+})
+
+app.listen(9090)
