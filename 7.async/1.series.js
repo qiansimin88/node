2146f5700869ac22db6cab121fa81a05786ba216
Qiansimin <qiansimin@shining3d.com>
2016-09-21 18:31:31 +0800

    fix

--- 7.async/1.series.js
+++ 7.async/1.series.js
@@ -1,12 +1,21 @@
 var async = require('async')
 var fs = require('fs')
+var path = require('path')
 var util = require('./util')
 var test = require('./text/3.test.js')
 
 async.series([
 	function (cb) {
-		util.readFile()
+		setTimeout(() => {
+			util.readFile(path.join(__dirname, 'text/1.txt'), cb)
+		}, 3000)
+	},
+	function (cb) {
+		setTimeout(() => {
+			util.readFile(path.join(__dirname, 'text/2.txt'), cb)
+		},1000)
 	}
 ], (err, data) => {
-
+	console.log(err)
+	console.log(data)
 })
